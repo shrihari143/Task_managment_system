e163622095af0763392465aa5417b1b162b32b20
shrihari143 <126378618+shrihari143@users.noreply.github.com>
2023-07-11 17:30:25 +0530

    final commit

---
+++ models/taskModel.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const transectionSchema = new mongoose.Schema(
+  {
+    userid: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: [true, "amount is required"],
+    },
+    type: {
+      type: String,
+      required: [true, "type is required"],
+    },
+    category: {
+      type: String,
+      requires: [true, "cat is required"],
+    },
+    refrence: {
+      type: String,
+    },
+    description: {
+      type: String,
+      required: [true, "desc is required"],
+    },
+    date: {
+      type: Date,
+      required: [true, "data is required"],
+    },
+    tittle: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      default: "pending",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const transectionModel = mongoose.model("transections", transectionSchema);
+module.exports = transectionModel;
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const path = require("path");
+const connectDb = require("./Config/connectDb");
+// config dot env file
+dotenv.config();
+
+//databse call
+connectDb();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+
+//routes
+//user routes
+app.use("/api/v1/users", require("./routes/userRoutes"));
+//transections routes
+app.use("/api/v1/transections", require("./routes/taskRoutes"));
+
+//static files
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+//port
+const PORT = 8000 || process.env.PORT;
+
+//listen server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
